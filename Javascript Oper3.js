1180415d9298370ebc16c31d2dfdf0ac532d6b21
SurendraJaiswar <147294538+SurendraJaiswar@users.noreply.github.com>
2023-12-20 18:53:26 +0530

    Add files via upload

---
+++ Javascript Oper3.js
@@ -0,0 +1,18 @@
+// Unary operatorso Unary operators operate on a single operand.
+
+// Binary operatorso Binary operators operate on two operands.
++
+-
+*
+/
+%
+==
+===
+!=
+!==
+>
+<
+>=
+<=
+&&
+||
